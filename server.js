57d51daf8eb6d4caf26e5611aaa0579ec32b14ef
Mathieu Florentin <mathieu.florentin27@gmail.com>
2022-06-17 16:13:37 +0200

    Sequelize init

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   console.log("Here");
   res.render("index", { text: "world" });
 });
 
-const userRouter = require("./routes/users");
+const userRouter = require("./app/routes/users");
 
 app.use("/users", userRouter);
+
+const db = require("./app/models");
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
 
 app.listen(3000);
